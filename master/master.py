6299732448b36916563f829cc9cfa2dd46b55aac
MichealKenny <KennyMataz@gmail.com>
2015-09-23 01:17:30 +0100

    Added proper login functionality

--- master/master.py
+++ master/master.py
@@ -1,4 +1,4 @@
-from bottle import route, run, static_file, auth_basic
+from bottle import route, run, static_file, request, response, redirect
 from requests import get, exceptions
 
 
@@ -8,15 +8,34 @@ server_db = [
     {'name': 'Slave 3', 'host': 'localhost', 'port': '29549'}]
 
 
-def login(username, password):
+@route('/login')
+def login():
+    if request.get_cookie('auth', secret='admin') == 'admin':
+        redirect('/')
+
+    return open('html/login.html', 'r').read()
+
+@route('/logout')
+def login():
+    response.set_cookie('auth', '', secret='admin', expires=0)
+    redirect('/')
+
+@route('/auth', method='POST')
+def auth():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
     if username == 'admin' and password == 'admin':
-        return True
+        response.set_cookie('auth', 'admin', secret='admin', max_age=1000)
+        redirect('/')
     else:
-        return False
+        redirect('/login')
+
 
 @route('/')
-@auth_basic(login)
 def index():
+    if request.get_cookie('auth', secret='admin') != 'admin':
+        redirect('/login')
+
     page = '<img src="images/header.png">'
 
     for server in server_db:
@@ -35,7 +54,7 @@ def index():
         except exceptions.ConnectionError:
             page += '<h3><a href="server/{name}">{name}</a> <img src="images/red.png" title="Server down"></h3>'.format(name=server['name'])
 
-    return page
+    return page + '<a href="/logout">Logout</a>'
 
 @route('/server/<name>')
 def server(name):
@@ -44,5 +63,9 @@ def server(name):
 @route('/images/<name>')
 def images(name):
     return static_file(name, root='images/')
+
+@route('/css/<name>')
+def images(name):
+    return static_file(name, root='css/')
 
 run(host='localhost', port=8883)
